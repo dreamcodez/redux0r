e9085e2ecfa12228de1b1cb6721fa05248acf59a
Matthew Elder <sseses@gmail.com>
2018-01-29 20:20:37 -0800

    wippy2

--- index.js
+++ index.js
@@ -11,20 +11,54 @@ const defaultState = freeze({
 
 function redux0r(state = defaultState, action) {
     switch (action.type) {
+        case 'LISTENING':
+            return freeze({
+                ...state,
+                listenStatus: 'up'
+            })
         case 'LISTEN':
             return freeze({
                 ...state,
                 listenStatus: 'initializing',
                 listenPort: action.port
             })
+        case 'SHUTDOWN':
+            return freeze({
+                ...state,
+                listenStatus: 'down',
+                listenPort: null
+            })
         default:
             return state
     }
 }
 
+
+let server
+function listening() {
+    return store.dispatch({ type: 'LISTENING' });
+}
+
 function listen(port) {
-    doListen(port)
-    return { type: 'LISTENING', port port };
+    const state = store.getState()
+    if (state.listenStatus === 'initializing') {
+        console.error('cannot listen while initializing')
+    } else {
+        doListen(port)
+        return store.dispatch({ type: 'LISTEN', port });
+    }
+}
+
+function shutdown() {
+    const state = store.getState()
+    if (state.listenStatus === 'up') {
+        server.destroy()
+        return store.dispatch({ type: 'SHUTDOWN' });
+    } else {
+        console.error('cannot shutdown unless listenStatus is \'up\'')
+        return
+    }
+
 }
 
 
@@ -32,16 +66,17 @@ function listen(port) {
 // https://www.npmjs.com/package/server-destroy
 function doListen(port) {
     const app = new Koa()
-    const server = http.createServer(app.callback())
-    server.listen(port)
+    server = http.createServer(app.callback())
+    server.listen(port, () => {
+        store.dispatch({ type: 'LISTENING' })
+    })
     destroyable(server)
-    console.log(`Listening on ${port}...`)
-    return server
 }
 
 const store = createStore(redux0r)
 
-store.subscribe(() =>
-  console.log(store.getState())
-)
-store.dispatch(listen(3000))
+store.subscribe(() => console.log(store.getState()))
+listen(3000)
+setTimeout(() =>{
+shutdown()
+}, 1000)
