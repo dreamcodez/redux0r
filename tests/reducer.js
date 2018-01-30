29ad45ce798b51d28785bf3db6c0833ddebf9639
Matthew Elder <sseses@gmail.com>
2018-01-29 19:56:21 -0800

    test framework

---
+++ tests/reducer.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import { expect } from 'chai'
+
+test('my passing test', t => {
+	t.pass()
+})
+
+test('my failing test', t => {
+    expect(true).to.be(false)
+})
+test('my failing test', t => {
+    expect(true).to.be(false)
+})
+test('my failing test', t => {
+    expect(true).to.be(false)
+})
+test('my failing test', t => {
+    expect(true).to.be(false)
+})
+test('my failing test', t => {
+    expect(true).to.be(false)
+})
