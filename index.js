00f96fc7724ee1712ec0cf1d0f847f1e4c6ba851
Matthew Elder <sseses@gmail.com>
2018-01-29 17:10:14 -0800

    woo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import http from 'http'
+import destroyable from 'server-destroy'
 import Koa from 'koa'
 import { createStore } from 'redux'
 import freeze from 'deep-freeze'
@@ -6,7 +8,6 @@ const defaultState = freeze({
     port: 3000
 })
 
-//const app = new Koa();
 function redux0r(state = defaultState, action) {
     switch (action.type) {
         case 'BIND_PORT':
@@ -24,3 +25,17 @@ store.subscribe(() =>
   console.log(store.getState())
 )
 store.dispatch({ type: 'LISTEN', port: 3000 })
+
+
+// https://github.com/koajs/koa/issues/659
+// https://www.npmjs.com/package/server-destroy
+function listen(port) {
+    const app = new Koa()
+    const server = http.createServer(app.callback())
+    server.listen(port)
+    destroyable(server)
+    console.log(`Listening on ${port}...`)
+    return server
+}
+
+listen(3000)
